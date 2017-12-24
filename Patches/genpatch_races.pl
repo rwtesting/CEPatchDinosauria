962e7fc1edb678f2193c1601a2949ce71ff9213d
rwtesting <34680499+rwtesting@users.noreply.github.com>
2017-12-24 04:18:20 -0800

    move to lib RWPatcher::Animals, temp remove stun attacks
    - Patch stun attacks out of Dinosauria animals to remove CE errors when parrying stun attack (https://github.com/NoImageAvailable/CombatExtended/issues/375)
    - Move patch functionality to RWPatcher::Animals library.

--- Patches/genpatch_races.pl
+++ Patches/genpatch_races.pl
@@ -2,46 +2,28 @@
 use strict;
 use warnings;
 
+use lib "../../_lib";
+use RWPatcher::Animals;
 use XML::Simple;
+use File::Basename qw(basename dirname);
 
 #
-# Generate patch to make Dinosauria races compatible with Combat Extended, b18.
+# Generate patch to make entitiesauria races compatible with Combat Extended, b18.
 #
 
-# Get dino tool melee bodyparts from mod source xml
-my $SOURCEFILE = "../../1136958577/Defs/ThingDefs_Races/Races_Animal_Dinosauria.xml";
-# print to this output patch file, overwrite existing
-my $OUTFILE = "./ThingDefs_Races/Races_Animal_Dinosauria.xml";
+# Get entity tool melee bodyparts from mod source xml
+# For each, patch file will be ./<base-dir-name>/<file.xml>
+# Source may end with (-REF)?.(txt|xml), which will be replaced with ".xml".
+my @SOURCEFILES = qw(
+    ../../1136958577/Defs/ThingDefs_Races/Races_Animal_Dinosauria.xml
+);
+my $SOURCEMOD = 'Dinosauria';  # Only patch if this mod is loaded (leave undefined to skip)
 
-# DEFAULT values not in source xml from Dinosauria
+# DEFAULT values not in source xml from entitiesauria
 my %DEFAULT = (
     bodyShape => "Quadruped",
     MeleeDodgeChance => 0.08,	# Elephant
     MeleeCritChance => 0.79,	# Elephant
-);
-
-# armor penetration DEFAULT per bodypart
-my $DEFAULT_AP = 0.15;		# default ap for unlisted bodyparts
-my %DEFAULT_AP = (
-    HeadAttackTool => 0.13,	# Elephant
-    TailAttackTool => 0.17,	# (like a leg?)
-
-    HornAttackTool => 0.457,	# Thrumbo (should differentiate between horncut/hornstab
-    Teeth          => 0.25,	# Smilodon
-    Beak           => 0.3,	# Titanis
-
-    FrontLeftLeg   => 0.17,	# Elephant
-    FrontRightLeg  => 0.17,	# Elephant
-
-    LeftLeg        => 0.17,
-    RightLeg       => 0.17,
-
-    LeftLegClawAttackTool  => 0.227,	# Megascarab headclaw
-    RightLegClawAttackTool => 0.227,	# Megascarab headclaw
-    LeftArmClawAttackTool  => 0.227,	# Megascarab headclaw
-    RightArmClawAttackTool => 0.227,	# Megascarab headclaw
-
-    
 );
 
 # CE values by type
@@ -53,10 +35,12 @@ my %VALS_MEDVEG   = (MeleeDodgeChance => 0.10, MeleeCritChance => 0.40); # like 
 my %VALS_SMVEG    = (MeleeDodgeChance => 0.29, MeleeCritChance => 0.22); # like Caribou
 my %VALS_TINY     = (MeleeDodgeChance => 0.45, MeleeCritChance => 0.15); # like ??
 
-# hash of values per dino
-# tools are bodyparts from <tools> entry in source xml
-# FIXME: Read tools bodyparts from source XML instead of hardcoding
-my %DINOS = (
+# hash of values per animal (using values from a17 MF+CE patch)
+# required: dodge, crit (not defined in vanilla)
+# optional: bodyShape (else default)
+# optional: any/all from @ARMORTYPES
+# optional: baseHealthScale
+my %PATCHABLES = (
 
     # Raptors - fast + hit hard - model after Enelodont a17
     TyrannosaurusRex => {
@@ -171,137 +155,15 @@ my %DINOS = (
     },
 );
 
-# Open source/output files
-# Open source/output files
-my $source =  XMLin($SOURCEFILE, ForceArray => [qw(ThingDef li)])
-    or die("ERR: read source xml $SOURCEFILE: $!\n");
-open(OUTFILE, ">", $OUTFILE)
-    or die("Failed to open/write $OUTFILE: $!\n");
+my $patcher = new RWPatcher::Animals(
+    sourcemod   => 'Dinosauria',
+    sourcefiles => \@SOURCEFILES,
+    cedata      => \%PATCHABLES,
+) or die("ERR: Failed new RWPatcher::Animals: $!\n");
 
-# Header 
-print OUTFILE (<<EOF);
-<?xml version="1.0" encoding="utf-8" ?>
-<Patch>
-
-  <!-- Warning: This will break if Dinosauria author moves dino defs to separate xml files.
-       You would need to create 1 PatchOperationSequence per xml file patched (or per dino)
-       (sequence = 1st xpath locates file for sequence = faster load times). -->
-
-  <!-- The script used to generate this file can easily be updated to 1 sequence per dino. -->
-
-  <Operation Class="PatchOperationSequence">
-  <success>Always</success>
-  <operations>
-
-EOF
-
-#
-# Step through source xml.
-# Generate a template for each $dino found.
-# If dino is found that we don't have CE values for, warn and skip.
-#
-# (use one sequence per file to reduce load times, short circuit)
-# Load times: Defs/ThingDef < /Defs/ThingDef << */ThingDef/ <<< //ThingDef/
-#
-my($dino, $tool, $ap, $bodyshape);
-foreach my $entry ( @{$source->{ThingDef}} )
-{
-    # Skip non-dinos and unknown dinos
-    next unless ($dino = $entry->{defName}) && $entry->{ParentName} eq "AnimalThingBase";
-
-    if (!exists $DINOS{$dino})
-    {
-        warn(<<EOF);
-WARN: New or unknown dino found. Skipping because no CE data:
-
-Name: $dino
-Desc:
-$entry->{description}
-
-EOF
-        next;
-    }
-
-    # Start patch
-    print OUTFILE (<<EOF);
-  <!-- ========== $dino ========== -->
-
-EOF
-
-    # For each bodypartgroup listed for this dino, add CE attribute + armor pen value
-    if ($entry->{tools}->{li})
-    {
-        foreach $tool ( @{ $entry->{tools}->{li} } )
-        {
-            $ap = $DEFAULT_AP{$tool->{linkedBodyPartsGroup}} || $DEFAULT_AP;
-            print OUTFILE (<<EOF);
-    <li Class="PatchOperationAttributeSet">
-    <xpath>Defs/ThingDef[defName="$dino"]/tools/li[linkedBodyPartsGroup="$tool->{linkedBodyPartsGroup}"]</xpath>
-        <attribute>Class</attribute>
-        <value>CombatExtended.ToolCE</value>
-    </li>
-
-    <li Class="PatchOperationAdd">
-    <xpath>Defs/ThingDef[defName="$dino"]/tools/li[linkedBodyPartsGroup="$tool->{linkedBodyPartsGroup}"]</xpath>
-    <value>
-        <armorPenetration>$ap</armorPenetration>
-    </value>
-    </li>
-
-EOF
-        }
-    }
-
-    # Add bodyShape and melee dodge/crit
-    $bodyshape = $DINOS{$dino}->{bodyShape} || $DEFAULT{bodyShape};
-    print OUTFILE (<<EOF);
-    <li Class="PatchOperationAddModExtension">
-    <xpath>Defs/ThingDef[defName="$dino"]</xpath>
-    <value>
-        <li Class="CombatExtended.RacePropertiesExtensionCE">
-            <bodyShape>$bodyshape</bodyShape>
-        </li>
-    </value>
-    </li>
-
-    <!-- List dodge/crit last so that we know all previous sequence entries succeeded.
-         These values are easy to check in-game. -->
-    <li Class="PatchOperationAdd">
-    <xpath>Defs/ThingDef[defName="$dino"]/statBases</xpath>
-    <value>
-        <MeleeDodgeChance>$DINOS{$dino}->{MeleeDodgeChance}</MeleeDodgeChance>
-        <MeleeCritChance>$DINOS{$dino}->{MeleeCritChance}</MeleeCritChance>
-    </value>
-    </li>
-
-EOF
-}
-
-# print closer
-print OUTFILE (<<EOF);
-  </operations> <!-- End sequence -->
-  </Operation>  <!-- End sequence -->
-
-</Patch>
-
-EOF
-close(OUTFILE) or warn("WARN: close $OUTFILE: $!\n");
+$patcher->generate_patches or die("ERR: generate_patches: $!\n");
 
 exit(0);
 
 __END__
-
-=head1 NOTES
-
-=head2 Defined in BodyPartGroups_Dinosaurs.xml
-
-    TailAttackTool
-    LeftArmClawAttackTool
-    RightArmClawAttackTool
-    LeftLegClawAttackTool
-    RightLegClawAttackTool
-    LeftLeg
-    RightLeg
-
-=cut
 
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Combat Extended compatibility patch for Dinosauria (b18).
 
+Note: Stun attacks from Dinosauria animals have temporarily been removed to avoid
+      CE issue https://github.com/NoImageAvailable/CombatExtended/issues/375
+
 Add values needed by CE, including melee dodge, melee crit, armor penetration, and body shape.
 
 Values are approximate, based on similar entities from the CE patch for Core animals and the a17 patch for Megafauna.  I categorized dinos based on the description and in-game pawn size, but the values may be off.
